e30bbc5c5234d497e1172f262b4120cad4f5a686
Adam Sayer <asayer82@icloud.com>
2024-08-22 16:00:42 +1000

    Added accesibility

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -53,6 +53,9 @@ struct ContentView: View {
                                 Image(systemName: "\(word.count).circle")
                                 Text(word)
                             }
+                            .accessibilityElement()
+                            .accessibilityLabel(word)
+                            .accessibilityHint("\(word.count) letters")
                         }
                     }
                    
